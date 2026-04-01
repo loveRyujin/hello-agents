d86c5a66008e3a5a71ee10c9b6d560ce5f3e2eb9
lichen <1725350830@qq.com>
2026-04-01 23:54:54 +0800

    docs: Update example prompt to use word problem
    - Change the example prompt from implementing a quicksort algorithm to solving a multi-step word problem about apple sales
    
    Signed-off-by: lichen <1725350830@qq.com>

--- plan_and_solve.py
+++ plan_and_solve.py
@@ -138,6 +138,6 @@ if __name__ == '__main__':
         llmClient = HelloAgentsLLM()
         
         agent = PlanAndSolveAgent(llmClient)
-        agent.run("实现一个快速排序算法")
+        agent.run("一个水果店周一卖出了15个苹果。周二卖出的苹果数量是周一的两倍。周三卖出的数量比周二少了5个。请问这三天总共卖出了多少个苹果？")
     except ValueError as e:
         print(e)
